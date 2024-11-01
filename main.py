cf2e8938976a5e40d7ae63b77995b56dd5d4a53d
Vadim Timakin <toefl@pop-os.localdomain>
2024-11-01 22:06:40 +0300

    Reduce memory usage

--- main.py
+++ main.py
@@ -404,6 +404,44 @@ class Dataset:
         
         return df
     
+    def reduce_mem_usage(self, df, float16_as32=True):
+        """Reduce memore usage."""
+
+        start_mem = df.memory_usage().sum() / 1024**2
+        print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+        for col in df.columns:
+            col_type = df[col].dtype
+
+            if col_type != object and str(col_type)!='category':
+                c_min,c_max = df[col].min(),df[col].max()
+
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)  
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        if float16_as32:
+                            df[col] = df[col].astype(np.float32)
+                        else:
+                            df[col] = df[col].astype(np.float16)  
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+        end_mem = df.memory_usage().sum() / 1024**2
+        print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+        print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+        return df
+    
     def save_data_checkpoint(self) -> None:
         """Save data checkpoint."""
         
@@ -420,6 +458,7 @@ class Dataset:
         df = self.build_validation_and_cv_features(df)
         df = self.drop_columns(df)
         df = self.postprocessing(df)
+        df = self.reduce_mem_usage(df)
         self.save_data_checkpoint()
         
         return df, self.data_checkpoint
@@ -634,6 +673,7 @@ class Solver:
         self.models[model_name]["oof_preds"] = oof_preds
         self.models[model_name]["oof_labels"] = oof_labels
         self.models[model_name]["mode"] = oof_mask
+        self.models[model_name]["fold"] = df["fold"].values
         
         print(f'\nCV scores {model_name}')
         for fold in range(len(scores)):
